0a5405d4dbe818bd24c34ad01d8779833fbb1a34
Jakub Both <jakub.both@uib.no>
2025-10-09 09:36:32 +0200

    add script for setting up color paths for each label

---
+++ src/setup_color_paths.py
@@ -0,0 +1,28 @@
+import argparse
+from pathlib import Path
+import logging
+
+from darsia.presets.workflows.rig import Rig
+from darsia.presets.workflows.setup_color_paths import setup_color_paths
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+if __name__ == "__main__":
+    # ! ---- PARSE DATA ----
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Setup run.")
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="config.toml",
+        help="Path to config file.",
+    )
+    parser.add_argument(
+        "--show", action="store_true", help="Show the labels after each step."
+    )
+    args = parser.parse_args()
+
+    setup_color_paths(Rig, Path(args.config), args.show)
