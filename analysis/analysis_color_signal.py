ba56e77ae1d72d1f4d6228bf513c25b70798bdd6
Jakub Both <jakub.both@uib.no>
2025-10-09 18:23:42 +0200

    add analysis for cropping and color signal

---
+++ analysis/analysis_color_signal.py
@@ -0,0 +1,30 @@
+"""This module exemmplifies the use of the calibrated color analysis."""
+
+import argparse
+from pathlib import Path
+import logging
+
+from darsia.presets.workflows.analysis.analysis_color_signal import (
+    analysis_color_signal,
+)
+from darsia.presets.workflows.rig import Rig
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Setup run.")
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="config.toml",
+        help="Path to config data.",
+    )
+    parser.add_argument(
+        "--show",
+        action="store_true",
+        help="Show the labels after each step.",
+    )
+    args = parser.parse_args()
+
+    analysis_color_signal(Rig, Path(args.config), args.show)
---
+++ analysis/analysis_cropping.py
@@ -0,0 +1,29 @@
+"""Batch analysis for cropping images."""
+
+import argparse
+from pathlib import Path
+
+import logging
+
+from darsia.presets.workflows.analysis.analysis_cropping import analysis_cropping
+from darsia.presets.workflows.rig import Rig
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Setup run.")
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="config.toml",
+        help="Path to config file.",
+    )
+    parser.add_argument(
+        "--show",
+        action="store_true",
+        help="Show the labels after each step.",
+    )
+    args = parser.parse_args()
+
+    analysis_cropping(Rig, Path(args.config), args.show)
