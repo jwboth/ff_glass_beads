f460ac61fb05495b226fefdb1165b324e507b2a1
Jakub Both <jakub.both@uib.no>
2026-01-22 03:37:23 +0100

    add roi to calibration script

--- scripts/calibration.py
+++ scripts/calibration.py
@@ -1,7 +1,7 @@
 """Full calibration for the FF Glass Beads project."""
 
 import logging
-
+import darsia
 from darsia.presets.workflows.rig import Rig
 from darsia.presets.workflows.user_interface_calibration import preset_calibration
 
@@ -9,4 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == "__main__":
-    preset_calibration(Rig)
+    rois = {
+        "full": darsia.CoordinateArray([(0.0, 0.0), (2.745, 1.5)]),
+    }
+    preset_calibration(Rig, rois=rois)
