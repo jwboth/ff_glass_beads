c88d699f38c577ef612b35c9e6b01833f3972458
Jakub Both <jakub.both@uib.no>
2026-04-09 09:08:53 +0200

    add entrypoint for utils

---
+++ scripts/utils.py
@@ -0,0 +1,10 @@
+"""Utils for the FF Glass Beads project."""
+
+import logging
+
+from darsia.presets.workflows.user_interface_utils import preset_utils
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    preset_utils()
